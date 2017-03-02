91215e7982756df7e376c83c32dce0e9d1e37648
Zachariah Ngonyani <zachariahn@gmail.com>
2017-03-02 22:04:33 +0300

    Format schema code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223110951) do
+ActiveRecord::Schema.define(version: 20170228202731) do
 
   create_table "docs", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20170223110951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_docs_on_user_id"
+  end
+
+  create_table "plags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
